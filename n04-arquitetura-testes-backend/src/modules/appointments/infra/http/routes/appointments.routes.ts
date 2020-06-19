9d835d7441fe07593bb43f93a0be1070231ddda1
Saron <saron.ph@gmail.com>
2020-06-18 23:33:16 -0300

    Imports

--- n04-arquitetura-testes-backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ n04-arquitetura-testes-backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,11 +2,11 @@ import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
 
-import AppointmentsRepository from '../modules/appointments/repositories/AppointmentsRepository';
+import AppointmentsRepository from '@modules/appointments/repositories/AppointmentsRepository';
 
-import CreateAppointmentServices from '../modules/appointments/services/CreateAppointmentServices';
+import CreateAppointmentServices from '@modules/appointments/services/CreateAppointmentServices';
 
-import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
 
--- n04-arquitetura-testes-backend/src/modules/appointments/infra/typeorm/entities/Appointment.ts
+++ n04-arquitetura-testes-backend/src/modules/appointments/infra/typeorm/entities/Appointment.ts
@@ -8,7 +8,7 @@ import {
   JoinColumn,
 } from 'typeorm';
 
-import User from './User';
+import User from '@modules/users/infra/typeorm/entities/User';
 
 // ManyToOne tipo de relacionamento, varios agendamentos para 1 provider
 
