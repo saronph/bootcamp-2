579686f73249a5de9553dc534467aaf5d1897a28
Saron <saron.ph@gmail.com>
2020-06-26 17:32:39 -0300

    Added emails tests

---
+++ n04-continuando-backend-app/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,42 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentRepository';
+import CreateAppointmentService from './CreateAppointmentServices';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '1332665',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('1332665');
+  });
+
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date();
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '1332665',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '1332665',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
---
+++ n04-continuando-backend-app/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,41 @@
+import { injectable, inject } from 'tsyringe';
+
+// import User from '@modules/users/infra/typeorm/entities/User';
+
+// import AppError from '@shared/errors/AppError';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IUsersTokenRepository from '../repositories/IUsersTokenRepository';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+
+    @inject('UsersTokenRepository')
+    private usersTokenRepository: IUsersTokenRepository,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exists');
+    }
+
+    await this.usersTokenRepository.generate(user.id);
+
+    this.mailProvider.sendMail(email, 'Pedido de recuperação de senha');
+  }
+}
+
+export default SendForgotPasswordEmailService;
