e7856e7da7e2932da30b44f46fa5f1db17b28fb6
Saron Medeiros Philippi <saron.ph@gmail.com>
2020-08-09 09:58:47 -0300

    add dashboard partial layout

--- n05-finalizando-front-end-web-do-app/src/pages/Dashboard/index.tsx
+++ n05-finalizando-front-end-web-do-app/src/pages/Dashboard/index.tsx
@@ -1,9 +1,124 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-// import { Container } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Section,
+  Appointment,
+  Calendar,
+} from './styles';
+
+import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
-  return <div />;
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const { signOut, user } = useAuth();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Bem vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars0.githubusercontent.com/u/54987514?s=460&u=d4e684ac6d5d19bbdec0b77e4a55e45af5c7c387&v=4"
+                alt="GoBarber"
+              />
+
+              <strong>Saron Medeiros Philippi</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/54987514?s=460&u=d4e684ac6d5d19bbdec0b77e4a55e45af5c7c387&v=4"
+                  alt="GoBarber"
+                />
+
+                <strong>Saron Medeiros Philippi</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/54987514?s=460&u=d4e684ac6d5d19bbdec0b77e4a55e45af5c7c387&v=4"
+                  alt="GoBarber"
+                />
+
+                <strong>Saron Medeiros Philippi</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/54987514?s=460&u=d4e684ac6d5d19bbdec0b77e4a55e45af5c7c387&v=4"
+                  alt="GoBarber"
+                />
+
+                <strong>Saron Medeiros Philippi</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+
+        <Calendar />
+      </Content>
+    </Container>
+  );
 };
 
 export default Dashboard;
